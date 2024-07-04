9025107a6976f11f537bfde2be5bdbdbe3514db6
Udi-ta <143646007+Udi-ta@users.noreply.github.com>
2024-07-05 01:35:56 +0530

    changes, without database

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,1 @@
-<h1>hello guyss</h1>
+<h1>This is the blog page...</h1>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,13 @@
-<script>
+<!-- <script>
     export let data
 </script>
-<p class="font- bold bg-blue-300">{data.content}</p>
+<p class="font- bold bg-blue-300">{data.content}</p> -->
+<!-- <script>
+    export let data;
+</script>
+
+<div>
+  {#each data.names as { name } }
+    <div class="font-semibold">{name}</div>
+  {/each}
+</div> -->
