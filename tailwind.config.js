058a88c6e6abf77336714f4c17dc664d2e57c282
Udi-ta <143646007+Udi-ta@users.noreply.github.com>
2024-07-06 17:54:34 +0530

    fullstack implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-// //   content: ['./src/**/*.{html,js,svelte,ts}'],
-//   content: [
-//     './src/**/*.html',
-//     './src/**/*.js',
-//     './src/**/*.svelte',
-//     './src/**/*.ts',
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
